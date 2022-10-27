561a5e9264e95547e537c468ba755562a739f01e
adrian.sebuliba <adrian.sebuliba@hotjar.com>
2022-10-27 07:19:45 +0300

    Create endpoints for application metadata V1

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,9 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
+
+from flask_marshmallow import Marshmallow
+
 
 import os
 
@@ -12,6 +15,7 @@ app.config[
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 class Application(db.Model):
@@ -43,10 +47,14 @@ class Application(db.Model):
             and self is not application
         ):
             self.children.append(application)
+            return True
+        return False
 
     def remove_dependent_app(self, application):
         if application.is_dependency_of(self):
             self.children.remove(application)
+            return True
+        return False
 
     def is_dependency_of(self, application):
         return self.parent_app == application
@@ -62,9 +70,117 @@ class Application(db.Model):
             Application.parent_id == self.parent_id, Application.id != self.id
         ).all()
 
+    def to_dict(self):
+        result = {
+            "name": self.name,
+            "version": self.version,
+            "stack": self.stack,
+            "description": self.description,
+            "team": self.team,
+            "owner": self.owner,
+            "eks_size": self.eks_size,
+        }
+
+        if self.get_child > 0:
+            holder = []
+            for child in self.children:
+                holder.append(child.name)
+                result["depends-on"] = holder
+
+        return result
+
     @property
     def depends_on(self):
         return self.children
 
     def __repr__(self):
         return f'<Application "{self.name}">'
+
+
+@app.route("/api/apps", methods=["GET"])
+def get_apps():
+    apps = [app.to_dict() for app in db.session.query(Application).all()]
+    return jsonify(apps)
+
+
+@app.route("/api/apps", methods=["POST"])
+def create_app():
+    body = request.get_json()
+    db.session.add(
+        Application(
+            name=body["name"],
+            version=body["version"],
+            stack=body["stack"],
+            description=body["description"],
+            team=body["team"],
+            owner=body["owner"],
+            eks_size=body["eks_size"],
+        )
+    )
+    db.session.commit()
+    return make_response(jsonify({"app": "success"}), 201)
+
+
+@app.route("/api/apps/<int:id>", methods=["GET"])
+def get_app(id):
+    app = Application.query.get(id)
+    return jsonify({"data": app.to_dict()})
+
+
+@app.route("/api/apps/<int:id>", methods=["PUT"])
+def update_app(id):
+    body = request.get_json()
+    db.session.query(Application).filter_by(id=id).update(
+        dict(
+            name=body["name"],
+            version=body["version"],
+            stack=body["stack"],
+            description=body["description"],
+            team=body["team"],
+            owner=body["owner"],
+            eks_size=body["eks_size"],
+        )
+    )
+    db.session.commit()
+    return jsonify({"success": True, "message": "application updated "})
+
+
+@app.route("/api/apps/<int:id>", methods=["DELETE"])
+def delete_app(id):
+    db.session.query(Application).filter_by(id=id).delete()
+    db.session.commit()
+    return jsonify({"success": True, "message": "application deleted "})
+
+
+@app.route("/api/apps/<int:id>/dependencies/<int:dependency_id>", methods=["POST"])
+def add_app_dependencies(id, dependency_id):
+    app = Application.query.get(id)
+    dep_app = Application.query.get(dependency_id)
+    if app.add_dependent_app(dep_app):
+        db.session.commit()
+        return (
+            jsonify({"success": True, "message": "dependency added"}),
+            201,
+        )
+    return jsonify({"success": False, "message": "dependency could not be added."}), 400
+
+
+@app.route("/api/apps/<int:id>/dependencies", methods=["GET"])
+def get_app_dependencies(id):
+    app = Application.query.get(id)
+
+    return jsonify(app.depends_on)
+
+
+@app.route("/api/apps/<int:id>/dependencies/<int:dependency_id>", methods=["DELETE"])
+def delete_app_dependency(id, dependency_id):
+    app = Application.query.get(id)
+    dep_app = Application.query.get(dependency_id)
+    if app.remove_dependent_app(dep_app):
+        db.session.commit()
+        return (
+            jsonify({"success": True, "message": "dependency removed"}),
+            200,
+        )
+
+    return jsonify({"success": False, "message": "dependency does not exist."}), 400
