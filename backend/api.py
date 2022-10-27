a9d37110b584b18635355fc675c96e315905d1ce
adrian.sebuliba <adrian.sebuliba@hotjar.com>
2022-10-27 04:00:42 +0300

    Add properties and methods to app model

--- backend/api.py
+++ backend/api.py
@@ -6,10 +6,13 @@ import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my secret"
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{os.environ['POSTGRES_PASSWORD']}@postgres:5432/{os.environ['POSTGRES_DB']}"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"postgresql://postgres:{os.environ['POSTGRES_PASSWORD']}@postgres:5432/{os.environ['POSTGRES_DB']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
 
 class Application(db.Model):
     __tablename__ = "applications"
@@ -29,9 +32,39 @@ class Application(db.Model):
         "Application", backref=db.backref("parent_app", remote_side=[id])
     )
 
-with app.app_context():
-    db.create_all()
+    @property
+    def get_child(self):
+        return len(self.children)
 
-@app.route("/api/test", methods=["GET"])
-def testing():
-    return jsonify({"status": "success", "message": "testing!"})
+    def add_dependent_app(self, application):
+        if (
+            not self.is_dependency_of(application)
+            and not application.is_dependency_of(self)
+            and self is not application
+        ):
+            self.children.append(application)
+
+    def remove_dependent_app(self, application):
+        if application.is_dependency_of(self):
+            self.children.remove(application)
+
+    def is_dependency_of(self, application):
+        return self.parent_app == application
+
+    def has_parent(self):
+        return bool(self.parent_app)
+
+    def is_parent(self):
+        return self.parent_id == self.id
+
+    def get_siblings(self):
+        return Application.query.filter(
+            Application.parent_id == self.parent_id, Application.id != self.id
+        ).all()
+
+    @property
+    def depends_on(self):
+        return self.children
+
+    def __repr__(self):
+        return f'<Application "{self.name}">'
